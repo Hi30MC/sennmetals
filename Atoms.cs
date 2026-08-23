dcf688570b3077ae7f071cf697dcd03bd8233656
Hi30MC <duimstraash@gmail.com>
2026-08-23 11:32:13 -0400

    atom textures

--- Atoms.cs
+++ Atoms.cs
@@ -10,40 +10,45 @@ public static class Atoms {
     }
 
     public static void LoadAtoms() {
-        typhor = Brimstone.API.CreateNormalAtom(
+        typhor = Brimstone.API.CreateMetalAtom(
             ID: 150,
             modName: "Sennmetals",
             name: "Typhor",
-            pathToSymbol: "textures/atoms/Hi30MC/Sennmetals/hestium_symbol",
-            pathToDiffuse: "textures/atoms/Hi30MC/Sennmetals/copper_diffuse"
+            pathToSymbol: "textures/atoms/Hi30MC/Sennmetals/typhor/typhor_difsym",
+            pathToLightramp: "textures/atoms/Hi30MC/Sennmetals/typhor/typhor_lightramp",
+            pathToRimlight: "textures/atoms/Hi30MC/Sennmetals/typhor/typhor_rimlight"
         );
-        sordi = Brimstone.API.CreateNormalAtom(
+        sordi = Brimstone.API.CreateMetalAtom(
             ID: 151,
             modName: "Sennmetals",
             name: "Sordi",
-            pathToSymbol: "textures/atoms/Hi30MC/Sennmetals/iridium_symbol",
-            pathToDiffuse: "textures/atoms/Hi30MC/Sennmetals/copper_diffuse"
+            pathToSymbol: "textures/atoms/Hi30MC/Sennmetals/sordi/sordi_difsym",
+            pathToLightramp: "textures/atoms/Hi30MC/Sennmetals/sordi/sordi_lightramp",
+            pathToRimlight: "textures/atoms/Hi30MC/Sennmetals/sordi/sordi_rimlight"
         );
-        eitros = Brimstone.API.CreateNormalAtom(
+        eitros = Brimstone.API.CreateMetalAtom(
             ID: 152,
             modName: "Sennmetals",
             name: "Eitros",
-            pathToSymbol: "textures/atoms/Hi30MC/Sennmetals/mitrum_symbol",
-            pathToDiffuse: "textures/atoms/Hi30MC/Sennmetals/copper_diffuse"
+            pathToSymbol: "textures/atoms/Hi30MC/Sennmetals/eitros/eitros_difsym",
+            pathToLightramp: "textures/atoms/Hi30MC/Sennmetals/eitros/eitros_lightramp",
+            pathToRimlight: "textures/atoms/Hi30MC/Sennmetals/eitros/eitros_rimlight"
         );
-        rofor = Brimstone.API.CreateNormalAtom(
+        rofor = Brimstone.API.CreateMetalAtom(
             ID: 153,
             modName: "Sennmetals",
             name: "Rofor",
-            pathToSymbol: "textures/atoms/Hi30MC/Sennmetals/azulum_symbol",
-            pathToDiffuse: "textures/atoms/Hi30MC/Sennmetals/copper_diffuse"
+            pathToSymbol: "textures/atoms/Hi30MC/Sennmetals/rofor/rofor_difsym",
+            pathToLightramp: "textures/atoms/Hi30MC/Sennmetals/rofor/rofor_lightramp",
+            pathToRimlight: "textures/atoms/Hi30MC/Sennmetals/rofor/rofor_rimlight"
         );
-        serket = Brimstone.API.CreateNormalAtom(
+        serket = Brimstone.API.CreateMetalAtom(
             ID: 154,
             modName: "Sennmetals",
             name: "Serket",
-            pathToSymbol: "textures/atoms/Hi30MC/Sennmetals/taceum_symbol",
-            pathToDiffuse: "textures/atoms/Hi30MC/Sennmetals/copper_diffuse"
+            pathToSymbol: "textures/atoms/Hi30MC/Sennmetals/serket/serket_difsym",
+            pathToLightramp: "textures/atoms/Hi30MC/Sennmetals/serket/serket_lightramp",
+            pathToRimlight: "textures/atoms/Hi30MC/Sennmetals/serket/serket_rimlight"
         );
 
         QApi.AddAtomType(typhor);
